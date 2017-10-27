b5b46498c4a403f0847ee7325bad8146a80d1314
xhli110@126.com <xhli110@126.com>
2017-10-27 19:59:21 +0800

    初始化

---
+++ load-balance/src/main/java/github/wingedfish/loadbalance/RequestDispatch.java
@@ -0,0 +1,89 @@
+package github.wingedfish.loadbalance;
+
+import java.util.*;
+
+/**
+ * Created by lixiuhai on 2017/10/26.
+ */
+public class RequestDispatch {
+
+    private static final String server1 = "";
+    private static final String server2 = "";
+    private static final String server3 = "";
+    private static final String server4 = "";
+    private static Map<String, Integer> serverMap = new HashMap<String, Integer>();
+
+    static {
+        serverMap.put(server1, 1);
+        serverMap.put(server2, 2);
+        serverMap.put(server3, 3);
+        serverMap.put(server4, 4);
+    }
+
+    private int position = 0;
+
+    public String getServerByRandom() {
+        Map<String, Integer> currentServerMap = serverMap;
+
+        List<String> serverList = new ArrayList<String>(currentServerMap.keySet());
+
+        int serverIndex = new Random().nextInt(serverList.size());
+        return serverList.get(serverIndex);
+    }
+
+    public String getServerByAssiginProportionRandom() {
+        Map<String, Integer> currentServerMap = serverMap;
+
+
+        List<String> serverList = new ArrayList<String>();
+
+        Set<Map.Entry<String, Integer>> entrySet = currentServerMap.entrySet();
+        for (Map.Entry<String, Integer> entry : entrySet) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                serverList.add(entry.getKey());
+            }
+        }
+
+        //随机取出
+        int serverIndex = new Random().nextInt(serverList.size());
+        return serverList.get(serverIndex);
+
+    }
+
+    public String getServerByAssiginProportionPoll() {
+        Map<String, Integer> currentServerMap = serverMap;
+
+        List<String> serverList = new ArrayList<String>();
+
+        Set<Map.Entry<String, Integer>> entrySet = currentServerMap.entrySet();
+        for (Map.Entry<String, Integer> entry : entrySet) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                serverList.add(entry.getKey());
+            }
+        }
+        String server;
+        synchronized (this) {
+            server = serverList.get(position);
+            position++;
+        }
+        return server;
+    }
+
+    public String getServerByAssiginProportion() {
+        Map<String, Integer> currentServerMap = serverMap;
+
+        Map<Integer, String> resultMap = new HashMap<Integer, String>();
+
+        Set<Map.Entry<String, Integer>> entrySet = currentServerMap.entrySet();
+        for (Map.Entry<String, Integer> entry : entrySet) {
+            int startIndex = entry.getValue();
+            for (int i = 1; i < entry.getValue() + 1; i++) {
+                resultMap.put(startIndex, entry.getKey());
+                startIndex++;
+            }
+        }
+
+        int serverIndex = new Random().nextInt(resultMap.size());
+        return resultMap.get(serverIndex);
+    }
+}
